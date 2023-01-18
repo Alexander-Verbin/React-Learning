5b320d6b12ceea03b87cdffe758a91f9e71dcc01
Alexander-Verbin <sash.verbin@yandex.ru>
2023-01-18 22:16:15 +0300

    refactoring

--- src/api/api.js
+++ src/api/api.js
@@ -1,45 +1,37 @@
 import axios from "axios";
 
 const baseURL = axios.create({
-  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-  withCredentials: true,
-  headers: {
-    'API-KEY': 'e9c013ae-2c87-4fde-adf1-22e6a9826cf8'
-  },
+	baseURL: 'https://social-network.samuraijs.com/api/1.0/', withCredentials: true, headers: {
+		'API-KEY': 'e9c013ae-2c87-4fde-adf1-22e6a9826cf8'
+	},
 });
 
 export const usersAPI = {
-  getUsers(param1,param2) {
-    return baseURL.get(`users?page=${param1}&count=${param2}`).then(response => response.data);
-  },
-  follow(userId) {
-    return baseURL.post(`follow/${userId}`);
-  },
-  unfollow(userId) {
-    return baseURL.delete(`follow/${userId}`);
-  },
+	getUsers(param1, param2) {
+		return baseURL.get(`users?page=${param1}&count=${param2}`).then(response => response.data);
+	}, follow(userId) {
+		return baseURL.post(`follow/${userId}`);
+	}, unfollow(userId) {
+		return baseURL.delete(`follow/${userId}`);
+	},
 };
 
 export const profileAPI = {
-  getProfile(userId){
-    return baseURL.get(`profile/${userId}`);
-  },
-  getStatus(userId){
-    return baseURL.get(`profile/status/${userId}`);
-  },
-  updateStatus(status){
-    return baseURL.put(`profile/status`, {status: status});
-  }
+	getProfile(userId) {
+		return baseURL.get(`profile/${userId}`);
+	}, getStatus(userId) {
+		return baseURL.get(`profile/status/${userId}`);
+	}, updateStatus(status) {
+		return baseURL.put(`profile/status`, {status: status});
+	}
 };
 
 export const authAPI = {
-  me(){
-    return baseURL.get(`auth/me`)
-  },
-  login(email, password, remeberMe = false){
-    return baseURL.post(`auth/login`, {email, password, remeberMe});
-  },
-  logout(){
-    return baseURL.delete(`auth/login`);
-  }
+	me() {
+		return baseURL.get(`auth/me`)
+	}, login(email, password, remeberMe = false) {
+		return baseURL.post(`auth/login`, {email, password, remeberMe});
+	}, logout() {
+		return baseURL.delete(`auth/login`);
+	}
 };
--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -3,24 +3,24 @@ import s from './Paginator.module.css'
 
 const Paginator = (props) => {
 
-    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+	let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-    let pages = [];
+	let pages = [];
 
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i);
-    }
+	for (let i = 1; i <= pagesCount; i++) {
+		pages.push(i);
+	}
 
-    return (
-        <div>
-            {pages.map(p => {
-                return <button className={props.currentPage === (p && s.selectedPage) || ''}
-                               onClick={(e) => {
-                                   props.onPageChenged(p)
-                               }}>{p}</button>
-            })}
-        </div>
-    );
+	return (
+		<div>
+			{pages.map(p => {
+				return <button className={props.currentPage === (p && s.selectedPage) || ''}
+											 onClick={(e) => {
+												 props.onPageChenged(p)
+											 }}>{p}</button>
+			})}
+		</div>
+	);
 };
 
 export default Paginator;
